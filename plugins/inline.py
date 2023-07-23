f6f7ca7ecc9958a141082586f3c7c6beb015647e
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-07-23 23:24:36 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -21,7 +21,7 @@ async def inline_users(query: InlineQuery):
 
 @Client.on_inline_query()
 async def answer(bot, query):
-    """Show search results for given inline query"""
+    """Search Results"""
     
     if not await inline_users(query):
         await query.answer(results=[],
@@ -33,7 +33,7 @@ async def answer(bot, query):
     if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You Must Join Channel, To Use Me.',
                            switch_pm_parameter="subscribe")
         return
 

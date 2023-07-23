d3c502ed9818c5f62fe1c418d63c67954b3d1ecb
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-07-23 22:05:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tsunemoriii/Request-Trial.git /Request-Trial
+  git clone https://github.com/Tsunemoriii/Faye-V1.git /Faye-V1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/Tsunemoriii/Request-Trial /Request-Trial
+  git clone https://github.com/Tsunemoriii/Faye /Faye-V1
 fi
-cd /Request-Trial
+cd /Faye-V1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

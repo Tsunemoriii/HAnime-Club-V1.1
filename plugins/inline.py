39926c62e591c38cc43f7b53f3b07412eb2bd4bb
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-07-23 23:25:19 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
         except Exception as e:
             logging.exception(str(e))
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} No results'
+        switch_pm_text = f'{emoji.CROSS_MARK} Dont Search Anime Here.'
         if string:
             switch_pm_text += f' for "{string}"'
 
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search Again', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
